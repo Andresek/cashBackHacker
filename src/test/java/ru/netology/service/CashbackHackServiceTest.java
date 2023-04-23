26f4d122c5ae869c950508cf95b2c15fb02ed038
Andrew S <samum_94@mail.ru>
2023-04-23 12:18:28 +0300

    renamed autotests

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -7,7 +7,7 @@ import static org.testng.Assert.*;
 public class CashbackHackServiceTest {
 
     @Test
-    public void testRemain1() {
+    public void whenAmountOverBoundary() {
 
         CashbackHackService cashbackHackService = new CashbackHackService();
         int expected = cashbackHackService.remain(1100);
@@ -17,11 +17,22 @@ public class CashbackHackServiceTest {
 
     }
     @Test
-    public void testRemain2() {
+    public void whenAmountMultiplyBoundary() {
 
         CashbackHackService cashbackHackService = new CashbackHackService();
         int expected = cashbackHackService.remain(2000);
         int actual = 0;
+
+        assertEquals(actual,expected);
+
+    }
+
+    @Test
+    public void whenAmountLessBoundary() {
+
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int expected = cashbackHackService.remain(950);
+        int actual = 50;
 
         assertEquals(actual,expected);
 
